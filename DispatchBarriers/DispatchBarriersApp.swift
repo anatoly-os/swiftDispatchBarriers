53ea7ca8406b7ce1edf8ea54277f85e78c4cfd3a
anatoly-os <osokingauss@gmail.com>
2022-10-13 17:45:08 +0300

    Initial Commit

---
+++ DispatchBarriers/DispatchBarriersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DispatchBarriersApp.swift
+//  DispatchBarriers
+//
+//  Created by anatoly on 13.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DispatchBarriersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
